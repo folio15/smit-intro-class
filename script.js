d4fd4132f0db8dc363802970f3482f1e42f54f88
Meeran Ali <azlanirfan98@gmail.com>
2024-12-15 21:02:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// alert("hello from Meeran Sheikh")
+
+
+// function subtractTwoNumbers(){
+//     let num1 = +prompt("enter first number");
+//     let num2 = +prompt("enter second number");
+
+//     document.getElementById("answer").innerHTML = num1 - num2 
+//     console.log("hello from meeran sheikh")
+// }
+
+// subtractTwoNumbers
+
+function welcomeUser(){
+    let age = document.getElementById("age").value
+    document.getElementById("answer").innerHTML = "your age" + age 
+    console.log(age )
+}
+welcomeUser()
